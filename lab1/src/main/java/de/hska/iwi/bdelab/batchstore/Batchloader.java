ae92cf5eb706f8774bf5e69e5332585e68f806ac
Florian Siegrist <siegriste@gmx.de>
2017-12-24 03:20:49 +0100

    Implement importPageviews()-Method in BatchLoader

--- lab1/src/main/java/de/hska/iwi/bdelab/batchstore/Batchloader.java
+++ lab1/src/main/java/de/hska/iwi/bdelab/batchstore/Batchloader.java
@@ -14,16 +14,28 @@ import de.hska.iwi.bdelab.schema.Page;
 import de.hska.iwi.bdelab.schema.PageView;
 import de.hska.iwi.bdelab.schema.Pedigree;
 import de.hska.iwi.bdelab.schema.UserID;
+import manning.tap.DataPailStructure;
 
 import org.apache.hadoop.fs.FileSystem;
 
+import com.backtype.hadoop.pail.Pail;
+
 public class Batchloader {
+
+	private Pail<Data>.TypedRecordOutputStream os;
 
 	private void readPageviewsAsStream() {
 		try {
 			URI uri = Batchloader.class.getClassLoader().getResource("pageviews.txt").toURI();
 			try (Stream<String> stream = Files.lines(Paths.get(uri))) {
-				stream.forEach(line -> writeToPail(getDatafromString(line)));
+				stream.forEach(line -> {
+					try {
+						writeToPail(getDatafromString(line));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				});
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -48,27 +60,29 @@ public class Batchloader {
 		Page page = new Page();
 		page.set_url(url);
 
-		PageView pageView = new PageView(uid, page, 12345);
+		int nonce = (int) (Math.random() * 10000);
+
+		PageView pageView = new PageView(uid, page, nonce);
 
 		DataUnit dataUnit = new DataUnit();
 		dataUnit.set_pageview(pageView);
 
 		Pedigree pedigree = new Pedigree(Integer.parseInt(time));
 
-		result.set_dataunit(dataUnit);
-		result.set_pedigree(pedigree);
+		result = new Data(pedigree, dataUnit);
 
 		return result;
 	}
 
-	private void writeToPail(Data data) {
+	private void writeToPail(Data data) throws IOException {
+		os.writeObject(data);
 	}
 
 	private void importPageviews() {
 
 		// change this to "true" if you want to work
 		// on the local machines' file system instead of hdfs
-		boolean LOCAL = true;
+		boolean LOCAL = false;
 
 		try {
 			// set up filesystem
@@ -81,13 +95,18 @@ public class Batchloader {
 			String masterPath = FileUtils.prepareMasterFactsPath(false, LOCAL);
 
 			// set up new pail and a stream
-			// ...
+			Pail<Data> tempPail = Pail.create(newPath, new DataPailStructure());
+			os = tempPail.openWrite();
 
 			// write facts to new pail
 			readPageviewsAsStream();
 
+			os.close();
+
 			// set up master pail and absorb new pail
-			// ...
+			
+			Pail<Data> masterPail = Pail.create(masterPath, new DataPailStructure());
+			masterPail.absorb(tempPail);
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -95,9 +114,7 @@ public class Batchloader {
 	}
 
 	public static void main(String[] args) {
-        Batchloader loader = new Batchloader();
-//        loader.importPageviews();
-        loader.getDatafromString(pageview)
-    	
-//    }
+		Batchloader loader = new Batchloader();
+		loader.importPageviews();
+	}
 }
